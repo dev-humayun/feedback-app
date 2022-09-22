de9d963b1134b4f184ddceb9fb6e97e7848742e1
dev-humayun <humayunasghar90@gmail.com>
2022-09-22 21:58:40 +0500

    Context and UseEffect

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import Card from './shared/Card';
-import {FaTimes} from 'react-icons/fa'
+import {FaTimes,FaEdit} from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-export default function FeedbackItem({item,handleDelete}) {
+export default function FeedbackItem({item}) {
+
+  const {deleteFeedback,editFeedback}=useContext(FeedbackContext)
   return (
     <Card >
      <div className="num-display">{item.rating}</div>
-     <button className='close' onClick={()=>handleDelete(item.id)}>
+     <button className='close' onClick={()=>deleteFeedback(item.id)}>
       <FaTimes color='purple'/>
+     </button>
+     <button className='edit' onClick={()=>editFeedback(item)}>
+      <FaEdit color='purple'/>
      </button>
      <div className="text-display">{item.text}</div>
     </Card> 
   )
 }
 
-FeedbackItem.propTypes={
-  item:PropTypes.object.isRequired,
-}
+// FeedbackItem.propTypes={
+//   item:PropTypes.object.isRequired,
+// }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-export default function FeedbackList({feedback,handleDelete}) {
+export default function FeedbackList() {
+    const {feedback}=useContext(FeedbackContext)
     console.log(feedback)
   return (
   
     <div className='feedback-list'>
         {
             feedback.map((item)=>(
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+                <FeedbackItem key={item.id} item={item}/>
             ))
         }
       
@@ -19,14 +22,15 @@ export default function FeedbackList({feedback,handleDelete}) {
 
 
 
-FeedbackList.propTypes={
-  feedback:PropTypes.arrayOf(
-    PropTypes.shape(
-      {
-        id:PropTypes.number.isRequired,
-        rating:PropTypes.number.isRequired,
-        text:PropTypes.string.isRequired,
-      }
-    )
-  )
-}
+// FeedbackList.propTypes={
+//   feedback:PropTypes.arrayOf(
+//     PropTypes.shape(
+//       {
+//         id:PropTypes.number.isRequired,
+//         rating:PropTypes.number.isRequired,
+//         text:PropTypes.string.isRequired,
+//       }
+//     )
+//   )
+
+// }
