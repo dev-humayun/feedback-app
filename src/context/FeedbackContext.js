3a419000c816925c9572963ef6278a234b40afdc
dev-humayun <humayunasghar90@gmail.com>
2022-09-24 22:52:23 +0500

    React feedback with context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,11 +1,27 @@
-import { createContext, useState,useEffect } from 'react'
+import { createContext, useState } from 'react'
 import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([])
+    const [feedback, setFeedback] = useState([
+        {
+            id: 1,
+            text: "This is Item 1",
+            rating: 10
+        },
+        {
+            id: 2,
+            text: "This is item 2",
+            rating: 9
+        },
+        {
+            id: 3,
+            text: "This is item 3",
+            rating: 7
+        }
+    ])
     const [feedbackEdit,setFeedbackEdit]=useState({
         item:{},
         edit:false
